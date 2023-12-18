bc405456e6515b58819069241812c53c5797c6a1
rideshnath-silicon <rideshnath.siliconithub@gmail.com>
2023-12-18 15:09:33 +0530

    added user controllers and models

--- dto/request.go
+++ dto/request.go
@@ -1,1 +1,14 @@
 package dto
+
+import "github.com/dgrijalva/jwt-go"
+
+type UserLoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type JwtClaim struct {
+	Email string
+	ID    int
+	jwt.StandardClaims
+}
