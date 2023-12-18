4679b7ec1166560ccd45e63ff890115e1f8d5af0
dwarkesh2810 <dwarkeshpatel.siliconithub@gmail.com>
2023-12-18 12:37:37 +0530

    blue print

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	_ "github.com/dwarkesh2810/golang-demo/routers"
+
+	"github.com/beego/beego/v2/client/orm"
+	beego "github.com/beego/beego/v2/server/web"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	orm.RegisterDriver("postgres", orm.DRPostgres)
+	orm.RegisterDataBase("default", "postgres", "user=root password=1234 dbname=postgres sslmode=disable")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
